15f493e8c3bf9aec932a8dd8e1e79d1d29eb63c2
Elisha-RG <egeoffr190@gmail.com>
2026-06-24 10:22:30 +0200

    Created search element from API to update city and temperature

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+function updateWeather(response) {
+  let temperatureValue = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let citySearch = document.querySelector("#identified-city");
+
+  citySearch.innerHTML = response.data.city;
+  temperatureValue.innerHTML = Math.round(temperature);
+}
+
+function findCity(city) {
+  let apiKey = "7841011o26476aa4cefbe36669ffb9tf";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(updateWeather);
+}
+
 function beginSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let citySearch = document.querySelector("#identified-city");
-  citySearch.innerHTML = searchInput.value;
+
+  findCity(searchInput.value);
 }
-
-
-
-city = 
-apiKey = "7841011o26476aa4cefbe36669ffb9tf";
-apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", beginSearch);
