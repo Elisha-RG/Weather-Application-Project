0fa648d27d06c38aeb9647f823f4f3c02b535ab3
Elisha-RG <egeoffr190@gmail.com>
2026-06-24 11:46:09 +0200

    Completed date, time and condition elements

--- index.js
+++ index.js
@@ -2,9 +2,40 @@ function updateWeather(response) {
   let temperatureValue = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let citySearch = document.querySelector("#identified-city");
+  let conditionDescription = document.querySelector("#condition");
+  let humidityLevel = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind");
+  let currentDate = document.querySelector("#date-and-time");
+  let date = new Date(response.data.time * 1000);
 
   citySearch.innerHTML = response.data.city;
+  currentDate.innerHTML = formatDate(date);
+  conditionDescription.innerHTML = response.data.condition.description;
+  humidityLevel.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeed.innerHTML = `${response.data.wind.speed} km/h`;
   temperatureValue.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function findCity(city) {
@@ -22,3 +53,5 @@ function beginSearch(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", beginSearch);
+
+findCity("Munich");
